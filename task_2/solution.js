dde64c3c0828ae105dce76123f4fa5f0945faeda
Anna Veselova <aannya@yandex.ru>
2022-08-10 18:49:59 +0300

    task_2

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,16 +6,20 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+    let shippingSum;
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
+if(productsSum == 0 || productsSum >= freeShippingMinSum){
+    shippingSum = 0;
+} else {
+    shippingSum = shippingPrice;
+}
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -29,10 +33,16 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
+let discountSum;
 
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
+if(productsSum >= discountMinSum){
+    discountSum = discountPart * productsSum;
+} else{
+    discountSum = 0;
+}
 
     // Конец решения задания №2.2.
 
